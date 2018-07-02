8f9e0f1603f1f4692020c4c2ccf7447d2c852969
HPoumeyrol <hugues.poumeyrol.pf1@gmail.com>
2018-07-02 12:20:44 +0200

    first test junit

---
+++ src/main/java/tests1/Smileys.java
@@ -0,0 +1,13 @@
+package tests1;
+
+import java.util.List;
+
+public class Smileys {
+
+	public static int countSmileys(List<String> arr) {
+		  return 0;
+		}
+	
+	
+	
+}
---
+++ src/test/java/SmileysTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import tests1.Smileys;
+
+public class SmileysTests {
+
+	@Test
+	public void countSmileysTestEmpty() {
+		Smileys.countSmileys(null);
+	}
+}
